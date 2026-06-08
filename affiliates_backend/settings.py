e2c8b265b54a63132ee46ff0d7b0246d49739a72
alkadelik <debola_adeola@yahoo.com>
2026-06-08 09:14:29 +0100

    updating db for railway

--- affiliates_backend/settings.py
+++ affiliates_backend/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -66,17 +67,23 @@ WSGI_APPLICATION = 'affiliates_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         # 'ENGINE': 'django.db.backends.sqlite3',
+#         # 'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'leyyow_affiliate',
+#         'USER': 'postgres', # your postgres username
+#         'PASSWORD': 'your_password',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'leyyow_affiliate',
-        'USER': 'postgres', # your postgres username
-        'PASSWORD': 'your_password',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    "default": dj_database_url.parse(
+        os.environ["DATABASE_URL"]
+    )
 }
 
 
