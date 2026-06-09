d7922dbd9b7fa94cbd1ee5e1a7ccf8d019ea9944
alkadelik <debola_adeola@yahoo.com>
2026-06-09 07:55:13 +0100

    edit email bug

--- accounts/task.py
+++ accounts/task.py
@@ -4,10 +4,10 @@ from accounts.emails import send_affiliate_invite, send_affiliate_welcome#, send
 
 
 @shared_task(bind=True, max_retries=3, default_retry_delay=60)
-def task_send_affiliate_invite(self, affiliate_id):
+def task_send_affiliate_invite(self, affiliate_id, invite_url):
     try:
         affiliate = Affiliate.objects.get(id=affiliate_id)
-        send_affiliate_invite(affiliate)
+        send_affiliate_invite(affiliate, invite_url)
     except Exception as exc:
         raise self.retry(exc=exc)
 
